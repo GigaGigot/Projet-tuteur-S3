c9c19e0537a7a27b0a435cecdfde29652f4ad4e2
Syvain Metayer <sylvain.metayer@etu.unilim.fr>
2014-12-11 23:55:24 +0100

    Symfony "clean"
    Suppression du bundle demo.
    Restera à définir nos bundles.

--- Groupe 5/PR2L-Symfony/src/PR2L/MainBundle/Controller/AdvertController.php
+++ Groupe 5/PR2L-Symfony/src/PR2L/MainBundle/Controller/AdvertController.php
@@ -1,15 +1,19 @@
 <?php
 
-// src/OC/PlatformBundle/Controller/AdvertController.php
+// src/PR2L/MainBundle/Controller/AdvertController.php
 
-namespace OC\PlatformBundle\Controller;
+namespace PR2L\MainBundle\Controller;
 
+//Penser a ce use :
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class AdvertController
+class AdvertController extends Controller
 {
     public function indexAction()
     {
-        return new Response("Hello World !");
+    	$content = $this->get('templating')->render('PR2LMainBundle:Advert:index.html.twig');
+    	//le ':' separe les 'dossiers'
+        return new Response($content);
     }
 }
