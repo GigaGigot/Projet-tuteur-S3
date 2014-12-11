39f8d85741dca61fcdc7889c4c2ea8c2e8c350da
Syvain Metayer <sylvain.metayer@etu.unilim.fr>
2014-12-11 15:20:44 +0100

    Ajout de la nouvelle BD
    Jeux d'essai compris

--- Groupe 5/Site PR2L/PR2L/pr2l.sql
+++ Groupe 5/Site PR2L/PR2L/pr2l.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 11 Décembre 2014 à 13:35
+-- Généré le :  Jeu 11 Décembre 2014 à 15:20
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_derniereConnexion` date NOT NULL COMMENT 'format date',
   `user_droit` varchar(30) NOT NULL COMMENT 'admin, lecture, lecture/ecriture',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Vider la table avant d'insérer `user`
@@ -52,7 +52,9 @@ TRUNCATE TABLE `user`;
 --
 
 INSERT INTO `user` (`user_id`, `user_login`, `user_mdp`, `user_nom`, `user_prenom`, `user_tel`, `user_email`, `user_adherent`, `user_typeCompte`, `user_roles`, `user_derniereConnexion`, `user_droit`) VALUES
-(16, 'admin', '99cf22cf484c8f21df160dedabb0f129325dd6ea', 'admin', 'admin', '0000000000', 'admin@pr2l.fr', 'o', 'administrateur', 'secretaire', '2014-12-11', 'admin');
+(0, 'admin', '99cf22cf484c8f21df160dedabb0f129325dd6ea', 'Root', 'Admin', '0000000000', 'admin@pr2l.fr', 'o', 'administrateur', 'directeur', '2014-12-11', 'admin'),
+(1, 'odubos', '5fb1a0ca5063dfaeedf1d98dc89f59032c92bef1', 'Dubos', 'Olivier', '0000000000', 'olivier.dubos@pr2l.fr', 'o', 'moderateur', 'directeur', '2014-12-11', 'lecture/ecriture'),
+(20, 'noredacteur', '77b83a0661cc700ccffdd7bddd9ff90dfaf60619', 'Pas redacteur', 'Pas redacteur', '0000000000', 'a@b.fr', 'o', 'adherent', 'secretaire', '2014-12-11', 'lecture');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
